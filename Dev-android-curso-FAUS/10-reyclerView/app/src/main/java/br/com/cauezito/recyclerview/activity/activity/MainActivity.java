57b182abbff52e689ae2bf14a36989b5c0a17faa
Cauê Santos <caah.sfc@live.com>
2020-09-07 14:34:31 -0300

    Configurando o adapter e gerando os filmes

--- Dev-android-curso-FAUS/10-reyclerView/app/src/main/java/br/com/cauezito/recyclerview/activity/activity/MainActivity.java
+++ Dev-android-curso-FAUS/10-reyclerView/app/src/main/java/br/com/cauezito/recyclerview/activity/activity/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.cauezito.recyclerview.R;
+import br.com.cauezito.recyclerview.activity.adapter.Adapter;
+import br.com.cauezito.recyclerview.activity.model.Filme;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> filmes = new ArrayList<Filme>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerView);
 
-        //Configurar adapter
+        //Cria a listagem de filmes
+        geraFilmes();
+
+        //Configurar adapter (pega os dados e monta uma visualização)
+        Adapter adapter = new Adapter(filmes);
 
         //Configurar RecyclerView
         RecyclerView.LayoutManager recycler = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(recycler);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void geraFilmes(){
+        Filme filme = new Filme("O mundo sombrio de Kolo", "Horror", "1998");
+        this.filmes.add(filme);
+
+        filme = new Filme("Indo embora para casa", "Comédia", "2020");
+        this.filmes.add(filme);
+
+        filme = new Filme("A múmia 5", "Terror", "2020");
+        this.filmes.add(filme);
+
+        filme = new Filme("Pocoyo", "Animação", "2018");
+        this.filmes.add(filme);
+
+        filme = new Filme("Super heróis", "Fantasia", "2020");
+        this.filmes.add(filme);
     }
 }
--- Dev-android-curso-FAUS/10-reyclerView/app/src/main/java/br/com/cauezito/recyclerview/activity/adapter/Adapter.java
+++ Dev-android-curso-FAUS/10-reyclerView/app/src/main/java/br/com/cauezito/recyclerview/activity/adapter/Adapter.java
@@ -1,5 +1,6 @@
 package br.com.cauezito.recyclerview.activity.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,30 +10,39 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.w3c.dom.Text;
 
+import br.com.cauezito.recyclerview.R;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.adapter_filmes, parent, false);
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.titulo.setText("Viagem macabra");
+        holder.genero.setText("Terror");
+        holder.ano.setText("2020");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView titulo;
         TextView ano;
         TextView genero;
-        
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            titulo = itemView.findViewById(R.id.txtTitulo);
+            ano = itemView.findViewById(R.id.txtAno);
+            genero = itemView.findViewById(R.id.txtGenero);
         }
     }
 }
