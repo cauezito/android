ea1a0104e0e2a5e83fe4844b4ac4d8e8121fb1ab
Cauê Santos <caah.sfc@live.com>
2020-09-08 20:11:55 -0300

    Finalizando o app cara ou coroa

--- Dev-android-curso-FAUS/11caraoucoroa/app/src/main/java/br/com/cauezito/jogo_cara_ou_coroa/MainActivity.java
+++ Dev-android-curso-FAUS/11caraoucoroa/app/src/main/java/br/com/cauezito/jogo_cara_ou_coroa/MainActivity.java
@@ -2,8 +2,13 @@ package br.com.cauezito.jogo_cara_ou_coroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnJogar;
@@ -13,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnJogar = findViewById(R.id.btnJogar);
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+                intent.putExtra("resultado", new Random().nextInt(2));
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- Dev-android-curso-FAUS/11caraoucoroa/app/src/main/java/br/com/cauezito/jogo_cara_ou_coroa/ResultadoActivity.java
+++ Dev-android-curso-FAUS/11caraoucoroa/app/src/main/java/br/com/cauezito/jogo_cara_ou_coroa/ResultadoActivity.java
@@ -2,9 +2,12 @@ package br.com.cauezito.jogo_cara_ou_coroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ResultadoActivity extends AppCompatActivity {
     private ImageView imgResultado;
@@ -16,5 +19,22 @@ public class ResultadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resultado);
         imgResultado = findViewById(R.id.imgResultado);
         btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Bundle resultado = getIntent().getExtras();
+        geraResultado(resultado.getInt("resultado"));
+
+    }
+
+    private void geraResultado(int numero){
+        if(numero == 0) {
+            imgResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+        System.out.println("NUMEROOOO => " + numero);
     }
 }
