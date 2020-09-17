544067014372fc129b4724e3a078745af4b912f2
Cauê Santos <caah.sfc@live.com>
2020-09-17 11:56:17 -0300

    Salvando e atualizando dados no firebase

--- Dev-android-curso-FAUS/14-Firebase/app/src/main/java/br/com/cauezito/firebase/MainActivity.java
+++ Dev-android-curso-FAUS/14-Firebase/app/src/main/java/br/com/cauezito/firebase/MainActivity.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import br.com.cauezito.firebase.model.Usuario;
+
 public class MainActivity extends AppCompatActivity {
+
+    //se refere a raíz da estrutura, a não ser que outra seja especificada
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //referencia.child("usuarios").child("1").child("nome").setValue("Cauê");
+
+        DatabaseReference usuarios = referencia.child("usuarios");
+        usuarios.child("1").setValue(criaUsuario());
+    }
+
+    private Usuario criaUsuario(){
+        usuario = new Usuario();
+        usuario.setNome("Cauê");
+        usuario.setSobrenome("Santos");
+        usuario.setIdade(23);
+
+        return usuario;
     }
 }
