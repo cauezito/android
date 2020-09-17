6ff20b772f55393af3a7670ad64fcd554207404b
Cauê Santos <caah.sfc@live.com>
2020-09-17 12:07:29 -0300

    Recuperando dados no firebase

--- Dev-android-curso-FAUS/14-Firebase/app/src/main/java/br/com/cauezito/firebase/MainActivity.java
+++ Dev-android-curso-FAUS/14-Firebase/app/src/main/java/br/com/cauezito/firebase/MainActivity.java
@@ -3,9 +3,13 @@ package br.com.cauezito.firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.cauezito.firebase.model.Usuario;
 
@@ -19,10 +23,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //referencia.child("usuarios").child("1").child("nome").setValue("Cauê");
 
+        /* Salvando dados */
+        //referencia.child("usuarios").child("1").child("nome").setValue("Cauê");
         DatabaseReference usuarios = referencia.child("usuarios");
         usuarios.child("1").setValue(criaUsuario());
+
+        /* Recuperando dados */
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override //dado alterado
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE", dataSnapshot.getValue().toString());
+            }
+
+            @Override //requisição cancelada
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private Usuario criaUsuario(){
